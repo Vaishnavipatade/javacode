afbb1cf1a9683d904275e46384ae78ed15ca3fb4
Vaishnavipatade <patadev943@gmail.com>
2024-11-11 20:47:28 +0530

    Add files via upload

---
+++ Acces_variable.java
@@ -0,0 +1,15 @@
+public class Acces_variable {
+    int x=20;
+    double c=9.1;
+    String y="JAVA PROGRAMING";
+    char m='t';
+    public static void main(String[] args) {
+        Acces_variable n=new Acces_variable();
+        System.out.println("intiger:"+n.x);
+        System.out.println("double :"+n.c);
+        System.out.println("string:"+n.y);
+        System.out.println("charcter is :"+n.m);
+        
+    }
+    
+}
